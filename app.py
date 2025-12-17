60acb8f902afb0ae622e85ee16d63fc8d8ec248e
khamdanmuhammad <muhammadchamdani34@gmail.com>
2025-12-18 00:53:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,24 @@ st.set_page_config(
 )
 
 # ================== CSS ==================
+st.set_page_config(
+    page_title="Analisis Sentimen Akulaku",
+    layout="wide"
+)
+
+st.markdown("""<style>...</style>""", unsafe_allow_html=True)
 st.markdown("""
-<style>
-.card {background:white;padding:20px;border-radius:14px;
-box-shadow:0 4px 10px rgba(0,0,0,.08);margin-bottom:20px}
-.pos{color:#16a34a;font-size:22px;font-weight:bold}
-.net{color:#6b7280;font-size:22px;font-weight:bold}
-.neg{color:#dc2626;font-size:22px;font-weight:bold}
-</style>
+<div class="card">
+<h1>📊 Sistem Analisis Sentimen Akulaku</h1>
+<p>Anti Error • Anti Kebalik • Siap Sidang • Siap Cloud</p>
+</div>
 """, unsafe_allow_html=True)
+menu = st.sidebar.selectbox(
+    "Menu",
+    ["Upload Dataset (Opsional)", "Prediksi Kalimat", "Dashboard"]
+)
+
+)
 
 # ================== NLTK ==================
 import nltk
