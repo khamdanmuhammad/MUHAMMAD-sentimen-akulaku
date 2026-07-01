c86c1b6f5862aba4cb37c047fdd06d7b607e555f
khamdanmuhammad <muhammadchamdani34@gmail.com>
2026-07-01 21:23:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -272,7 +272,10 @@ elif menu == "🧠 Modeling & Evaluasi":
         y_pred = model.predict(X_test)
 
         st.success(f"Akurasi: {accuracy_score(y_test, y_pred):.4f}")
-        st.text(classification_report(y_test, y_pred))
+       # st.text(classification_report(y_test, y_pred))
+        laporan = laporan_klasifikasi(y_test, y_pred, output_dict=True)
+        df_laporan = pd.DataFrame(laporan).transpose()
+        st.table(df_laporan)
 
 # ================== DOWNLOAD ==================
 elif menu == "⬇️ Download CSV":
